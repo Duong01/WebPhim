0a6f0b4a98b005bd7ea8e2e687e49ec1dd15e179
Duong01 <nguyenvanduong10a5hh4@gmail.com>
2025-10-04 16:36:26 +0700

    Upload

--- src/pages/MoiveDetail.vue
+++ src/pages/MoiveDetail.vue
@@ -633,14 +633,14 @@ export default {
               // this.GetComment()
               // .then(resolve)
               // .catch(reject);
-              for(var j=0;j<result.episodes.length;j++){
+              for(let j=0;j<result.episodes.length;j++){
                 if((result.episodes[j].server_data.link_embed == "" || result.episodes[j].server_data.length <=0) && (result.episodes[result.episodes.length-1].server_data.link_embed == "" || result.episodes[result.episodes.length-1].server_data.length <=0)){
                   window.open('https://rophiim.me/?s='+this.slug);
                 }
               }
               resolve(true);
             } else {
-              for(var j=0;j<result.episodes.length;j++){
+              for(let j=0;j<result.episodes.length;j++){
                 if((result.episodes[j].server_data.link_embed == "" || result.episodes[j].server_data.length <=0) && (result.episodes[result.episodes.length-1].server_data.link_embed == "" || result.episodes[result.episodes.length-1].server_data.length <=0)){
                   window.open('https://rophiim.me/?s='+this.slug);
                 }
@@ -731,7 +731,7 @@ export default {
               // this.GetComment()
               // .then(resolve)
               // .catch(reject);
-              for(var j=0;j<result.episodes.length;j++){
+              for(let j=0;j<result.episodes.length;j++){
                 if((result.episodes[j].server_data.link_embed == "" || result.episodes[j].server_data.length <=0) && (result.episodes[result.episodes.length-1].server_data.link_embed == "" || result.episodes[result.episodes.length-1].server_data.length <=0)){
                   window.open('https://rophiim.me/?s='+this.slug);
                 }
