be3dd9a87b65cb71d113c1995dd869ddf3fa66c6
Duong01 <nguyenvanduong10a5hh4@gmail.com>
2025-10-04 17:11:04 +0700

    Upload

--- src/pages/MoiveDetail.vue
+++ src/pages/MoiveDetail.vue
@@ -637,7 +637,7 @@ export default {
                 ep.server_data.every(s => !s.link_embed && !s.link_m3u8)
               );
               if (isEmpty) {
-                window.location.href = 'https://rophiim.me/?s=' + this.slug;
+                window.location.href = 'https://rophiim.me/?s=' + this.movie.title;
               }
               // for(let j=0;j<result.episodes.length;j++){
               //   if((result.episodes[j].server_data[0].link_embed == "" || result.episodes[j].server_data[0].length <=0) && (result.episodes[result.episodes.length-1].server_data[0].link_embed == "" || result.episodes[result.episodes.length-1].server_data[0].length <=0)){
@@ -650,7 +650,7 @@ export default {
                 ep.server_data.every(s => !s.link_embed && !s.link_m3u8)
               );
               if (isEmpty) {
-                window.location.href = 'https://rophiim.me/?s=' + this.slug;
+                window.location.href = 'https://rophiim.me/?s=' + this.movie.title;
               }
               // for(let j=0;j<result.episodes.length;j++){
               //   if((result.episodes[j].server_data[0].link_embed == "" || result.episodes[j].server_data[0].length <=0) && (result.episodes[result.episodes.length-1].server_data[0].link_embed == "" || result.episodes[result.episodes.length-1].server_data[0].length <=0)){
@@ -746,7 +746,7 @@ export default {
               // .catch(reject);
               for(let j=0;j<result.episodes.length;j++){
                 if((result.episodes[j].server_data.link_embed == "" || result.episodes[j].server_data.length <=0) && (result.episodes[result.episodes.length-1].server_data.link_embed == "" || result.episodes[result.episodes.length-1].server_data.length <=0)){
-                  window.location.href = 'https://rophiim.me/?s='+this.slug;
+                  window.location.href = 'https://rophiim.me/?s='+this.movie.title;
 
                 }
               }
