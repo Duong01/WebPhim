1598aa65bcdbb1afe6c04e22b3324a1a2b93d40d
Duong01 <nguyenvanduong10a5hh4@gmail.com>
2025-11-13 20:08:10 +0700

    Upload

--- src/pages/MoiveDetail.vue
+++ src/pages/MoiveDetail.vue
@@ -1230,7 +1230,7 @@ export default {
   }
   .video-wrapper {
     width: 100%;
-    padding: 0 40px;
+    /* padding: 0 40px; */
     /* max-height: 800px; */
     aspect-ratio: 16 / 9;
     background: black;
@@ -1245,7 +1245,7 @@ export default {
 .video-wrapper video {
   width: 100%;
   height: 100%;
-  object-fit: cover;
+  object-fit: contain;
 }
 .suggested-item {
   cursor: pointer;
