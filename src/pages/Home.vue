2695c5f1012e83d606d2a9a21cfcc5ff2c5b898a
Duong01 <nguyenvanduong10a5hh4@gmail.com>
2025-11-24 11:27:18 +0700

    Upload

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -529,8 +529,8 @@ export default {
     const hours   = Math.floor(minutes / 60);
     const days    = Math.floor(hours / 24);
 
-    if (days > 0) return `${days} ngày trước`;
-    if (hours > 0) return `${hours} giờ trước`;
+    if (days > 0) return `${days} ngày ${hours} giờ trước`;
+    if (hours > 0) return `${hours} giờ ${minutes} phút trước`;
     if (minutes > 0) return `${minutes} phút trước`;
     return "Sắp chiếu";
   },
